9a18b734538feb065d2bd44c94c5b4ee3770d0c4
Ozziur <casaruizzo@yahoo.it>
2024-04-16 19:22:28 +0200

    Update user model fields and serializer in back app

--- back/back/urls.py
+++ back/back/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
 ]
--- back/back/views.py
+++ back/back/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
 from .serializer import UserSerializer
+from .models import User
 
 class RegisterView(APIView):
     def post(self, request):
@@ -9,3 +11,17 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = User.objects.filter(username=username).first()
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+        
+        userJson = UserSerializer(user).data #da capire
+        return Response(userJson, status=status.HTTP_200_OK)
