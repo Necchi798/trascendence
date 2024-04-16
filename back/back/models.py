2719f0177ef8336b0aef33210529efb557f37c32
Ozziur <casaruizzo@yahoo.it>
2024-04-16 17:47:47 +0200

    Update .gitignore and add initial models and settings in back app

--- back/back/models.py
+++ back/back/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    nick = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
 
-    USERNAME_FIELD = 'nick'
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
     
 
---
+++ back/back/views.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import UserSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
