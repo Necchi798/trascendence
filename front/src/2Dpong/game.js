935a809fbdfc86fecb529cb05bcd70a29585841b
giorgiacucino <73819212+giorgiacucino@users.noreply.github.com>
2024-05-11 15:19:56 +0200

    added score to game and changed to vel and alpha

--- front/src/2Dpong/game.js
+++ front/src/2Dpong/game.js
@@ -5,43 +5,9 @@ var ctx;
 var width;
 var height;
 var canvas;
-
-function windowResized()
-{
-	const pixelRatio = window.devicePixelRatio || 1;
-	canvas.width  = pixelRatio * canvas.clientWidth;
-	canvas.height = pixelRatio * canvas.clientHeight;
-	
-	var scaleX = canvas.width / width;
-	var scaleY = canvas.height / height;
-	
-	width = canvas.width;
-	height = canvas.height;
-
-	var x = p1.x * scaleX;
-	var y = p1.y * scaleY;
-	p1.resize(x, y, height / 20, height / 5);
-
-	x = p2.x * scaleX;
-	y = p2.y * scaleY;
-	p2.resize(x, y, height / 20, height / 5);
-
-	x = ball.x * scaleX;
-	y = ball.y * scaleY;
-	var velX = ball.velX * scaleX;
-	var velY = ball.velY * scaleY;
-	ball.resize(x, y, velX, velY, 20);
-}
-
-function random(min, max)
-{
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function randomRGB()
-{
-	return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
+var p1Score = 0;
+var p2Score = 0;
+var scale;
 
 // Define variables to keep track of player movements
 let player1Up = false;
@@ -49,33 +15,37 @@ let player1Down = false;
 let player2Up = false;
 let player2Down = false;
 let paused = false;
-let savedVelX = 0;
-let savedVelY = 0;
+let savedVel = 0;
+let savedAlpha = 0;
+let p1Offense = true;
+let p2Offense = false;
+let acceleration = 5;
+let isAccelerated = false;
 
 document.addEventListener('keydown', function(event) {
-    if (event.key === ' ') {
+	if (event.key === ' ') {
 		paused = !paused;
 		if (paused) {
-			savedVelX = ball.velX;
-			savedVelY = ball.velY;
-			ball.velX = 0;
-			ball.velY = 0;
+			savedVel = ball.vel;
+			savedAlpha = ball.alpha;
+			ball.vel = 0;
+			ball.alpha = 0;
 		}
 		else {
-			ball.velX = savedVelX;
-			ball.velY = savedVelY;
+			ball.vel = savedVel;
+			ball.alpha = savedAlpha;
 		}
 	}
 });
 
-// Add event listeners for player 1 movement
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'w') {
-        player1Up = true;
-    } else if (event.key === 's') {
-        player1Down = true;
-    }
-});
+document.addEventListener('keydown', function(event)
+{
+	if (event.key === 'w')
+		player1Up = true;
+	else if (event.key === 's')
+		player1Down = true;
+}
+);
 
 document.addEventListener('keyup', function(event) {
     if (event.key === 'w') {
@@ -86,67 +56,65 @@ document.addEventListener('keyup', function(event) {
 });
 
 // Add event listeners for player 2 movement
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'ArrowUp') {
-        player2Up = true;
-    } else if (event.key === 'ArrowDown') {
-        player2Down = true;
-    }
+document.addEventListener('keydown', function(event)
+{
+	if (event.key === 'ArrowUp') {
+		player2Up = true;
+	} else if (event.key === 'ArrowDown') {
+		player2Down = true;
+	}
 });
 
-document.addEventListener('keyup', function(event) {
-    if (event.key === 'ArrowUp') {
-        player2Up = false;
-    } else if (event.key === 'ArrowDown') {
-        player2Down = false;
-    }
+document.addEventListener('keyup', function(event)
+{
+	if (event.key === 'ArrowUp') {
+		player2Up = false;
+	} else if (event.key === 'ArrowDown') {
+		player2Down = false;
+	}
 });
 
 // Update player positions based on key states
 function updatePlayers() {
-	const offset = 10;
-    if (player1Up) {
-        if (p1.y - offset + p1.height < height && p1.y - offset > 0)
+	if (player1Up)
+	{
+		if (p1.y - p1.offsetmove + p1.height < height && p1.y - p1.offsetmove > 0)
 		{
-			p1.move(-offset);
+			p1.move(-p1.offsetmove);
 		}
-    }
-    if (player1Down) {
-        if (p1.y + offset + p1.height < height && p1.y + offset > 0)
+	}
+	if (player1Down)
+	{
+		if (p1.y + p1.offsetmove + p1.height < height && p1.y + p1.offsetmove > 0)
 		{
-			p1.move(offset);
+			p1.move(p1.offsetmove);
 		}
-    }
-    if (player2Up) {
-        if (p2.y - offset + p2.height < height && p2.y - offset > 0)
+	}
+	if (player2Up)
+	{
+		if (p2.y - p2.offsetmove + p2.height < height && p2.y - p2.offsetmove > 0)
 		{
-			p2.move(-offset);
+			p2.move(-p2.offsetmove);
 		}
-    }
-    if (player2Down) {
-        if (p2.y + offset + p2.height < height && p2.y + offset > 0)
+	}
+	if (player2Down)
 		{
-			p2.move(offset);
+		if (p2.y + p2.offsetmove + p2.height < height && p2.y + p2.offsetmove > 0)
+		{
+			p2.move(p2.offsetmove);
 		}
-    }
-	if (paused) {
-		player1Up = false;
-		player1Down = false;
-		player2Up = false;
-		player2Down = false;
-		ball.velX = 0;
-		ball.velY = 0;
 	}
 }
 
 class Player {
-	constructor(x, y, color, width, height)
+	constructor(x, y, width, height)
 	{
 		this.x = x;
 		this.y = y;
-		this.color = color;
+		this.color = "white";
 		this.width = width;
 		this.height = height;
+		this.offsetmove = this.height / 10;
 	}
 	draw()
 	{
@@ -169,12 +137,12 @@ class Player {
 }
 
 class Ball {
-	constructor(x, y, velX, velY, color, size)
+	constructor(x, y, vel, alpha, color, size)
 	{
 		this.x = x;
 		this.y = y;
-		this.velX = velX;
-		this.velY = velY;
+		this.vel = vel;
+		this.alpha = alpha;
 		this.color = color;
 		this.size = size;
 	}
@@ -183,70 +151,227 @@ class Ball {
 	{
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
-		ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+		ctx.rect(this.x, this.y, this.size, this.size);
 		ctx.fill();
+	}
+
+	start()
+	{
+		this.x = width / 2 - height / 40;
+		this.y = height / 2 - height / 40;
+		this.vel = 0;
+		this.alpha = 0;
 	}
 
 	update()
 	{
 		if (this.x + this.size >= width)
 		{
-			this.velX = -Math.abs(this.velX);
+			alert("p1 score!");
+			p1Score++;
+			paused = true;
+			isAccelerated = false;
+			player1Down = false;
+			player1Up = false;
+			player2Down = false;
+			player2Up = false;
+			p1Offense = true;
+			p2Offense = false;
+			this.start();
+			savedVel = 10;
+			savedAlpha = -(Math.PI / 4);
 		}
 
-		if (this.x - this.size <= 0)
+		else if (this.x <= 0)
 		{
-			this.velX = Math.abs(this.velX);
+			alert("p2 score!");
+			p2Score++;
+			isAccelerated = false;
+			paused = true;
+			player1Down = false;
+			player1Up = false;
+			player2Down = false;
+			player2Up = false;
+			p2Offense = true;
+			p1Offense = false;
+			this.start();
+			savedVel = 10;
+			savedAlpha = Math.PI + Math.PI / 4;
 		}
 
 		if (this.y + this.size >= height)
 		{
-			this.velY = -Math.abs(this.velY);
+			this.alpha = -this.alpha;
 		}
 
-		if (this.y - this.size <= 0)
+		else if (this.y <= 0)
 		{
-			this.velY = Math.abs(this.velY);
+			this.alpha = -this.alpha;
 		}
-		this.x += this.velX;
-		this.y += this.velY;
+	
+		var velX = this.vel * Math.cos(this.alpha);
+		var velY = this.vel * Math.sin(this.alpha);
+
+		this.x += velX;
+		this.y += velY;
 	}
-	resize(x, y, velX, velY, size)
+	resize(x, y, vel, alpha, size)
 	{
 		this.x = x;
 		this.y = y;
-		this.velX = velX;
-		this.velY = velY;
+		this.vel = vel;
+		this.alpha = alpha;
 		this.size = size;
 	}
 	collisionDetect()
 	{
-		if (this.x - this.size <= p1.x + p1.width && this.y >= p1.y && this.y <= p1.y + p1.height)
+		if (p2Offense && this.x <= p1.x + p1.width && this.y >= p1.y && this.y <= p1.y + p1.height)
 		{
-			this.velX = Math.abs(this.velX);
+			this.alpha = Math.PI - this.alpha;
+			if ((player1Down || player1Up) && !isAccelerated)
+			{	
+				this.vel += acceleration;
+				isAccelerated = true;
+			}
+			else if (isAccelerated)
+			{
+				this.vel -= acceleration;
+				isAccelerated = false;
+			}
+			p2Offense = false;
+			p1Offense = true;
 		}
-		if (this.x + this.size >= p2.x && this.y >= p2.y && this.y <= p2.y + p2.height)
+		if (p1Offense && this.x + this.size >= p2.x && this.y >= p2.y && this.y <= p2.y + p2.height)
 		{
-			this.velX = -Math.abs(this.velX);
+			//console.log("before p2: " + this.velX);
+			this.alpha = Math.PI - this.alpha;
+			console.log("p2: " + (player2Down || player2Up));
+			console.log("isAccelerated: " + isAccelerated);
+			if ((player2Down || player2Up) && !isAccelerated)
+			{	
+				this.vel += acceleration;
+				isAccelerated = true;
+			}
+			else if (isAccelerated)
+			{
+				this.vel -= acceleration;
+				isAccelerated = false;
+			}
+			//console.log("after p2: " + this.velX);
+			p2Offense = true;
+			p1Offense = false;
+			//console.log("p1Offense: " + p1Offense);
+			//console.log("p2Offense: " + p2Offense);
 		}
 	}
 }
 
+function drawScore(line_width, x, y, score)
+{
+	ctx.fillStyle = "white";
+
+	if (score == 0)
+		ctx.fillRect(x, y, line_width, 4 * line_width); //1
+	
+	ctx.fillRect((3 * line_width) + x, y, line_width, 4 * line_width); //2
+
+	if (score == 0 || score == 2)
+		ctx.fillRect(x, (3 * line_width) + y, line_width, 4 * line_width); //3
+
+	if (score == 0 || score == 1 || score == 3)
+		ctx.fillRect((3 * line_width) + x, (3 * line_width) + y, line_width, 4 * line_width); //4
+
+	if (score == 0 || score == 2 || score == 3)
+		ctx.fillRect(x, y, 4 * line_width, line_width); //5
+
+	if (score == 2 || score == 3)
+		ctx.fillRect(x, (3 * line_width) + y, 4 * line_width, line_width); //6
+	
+	if (score == 0 || score == 2 || score == 3)
+		ctx.fillRect(x, (6 * line_width) + y, 4 * line_width, line_width); //7
+}
+
 function loop()
 {
-	ctx.fillStyle = "rgba(0, 0, 0, 0.50)";
+	ctx.fillStyle = "rgba(0, 0, 0, 0.75)";
 	ctx.fillRect(0, 0, width, height);
+	ctx.fillStyle = "white";
+	var line_width = ball.size / 2;
+	var line_start = 0;
+	while (line_start < height)
+	{
+		ctx.fillRect(width / 2 - line_width / 2, line_start, line_width, line_width * 2);
+		line_start = line_start + line_width * 3;
+	}
+
+	drawScore(line_width, width / 4, 0, p1Score);
+	drawScore(line_width, 3 * width / 4, 0, p2Score);
 
 	updatePlayers();
-	ball.update();
-	ball.collisionDetect();
+	if (!paused)
+	{
+		ball.update();
+		ball.collisionDetect();
+	}
+	if (p1Offense)
+		ball.color = "red";
+	else if (p2Offense)
+		ball.color = "blue";
+	else
+		ball.color = "white";
 	ball.draw();
 	p1.draw();
 	p2.draw();
 	requestAnimationFrame(loop);
 }
 
-export function makeGame()
+function drawCanvas()
+{
+	var playerWidth = height / 20;
+	var playerHeight = height / 5;
+	var startingP1X = width / 30;
+	var startingPY = height / 2 - playerHeight / 2;
+	var startingP2X = width - startingP1X - playerWidth;
+	var ballSize = height / 20;
+	var startingBallX = width / 2 - ballSize / 2;
+	var startingBallY = height / 2 - ballSize / 2;
+
+	p1.resize(startingP1X, startingPY, playerWidth, playerHeight);
+	p2.resize(startingP2X, startingPY, playerWidth, playerHeight);
+	ball.resize(startingBallX, startingBallY, 0, 0, ballSize);
+
+	ctx.fillStyle = "rgba(0, 0, 0, 1)";
+	ctx.fillRect(0, 0, width, height);
+	p1.draw();
+	p2.draw();
+	ball.draw();
+}
+
+function resizeCanvas() {
+	var tempWidth = window.screen.availWidth < window.innerWidth ? window.screen.availWidth : window.innerWidth;
+	var tempHeight = window.screen.availHeight < window.innerHeight ? window.screen.availHeight : window.innerHeight;
+
+	if (tempWidth < 1.5 * tempHeight)
+	{
+		canvas.width = tempWidth;
+		canvas.height = tempWidth / 1.5;
+	}
+	else
+	{
+		canvas.width = 1.5 * tempHeight;
+		canvas.height = tempHeight;
+	}
+
+	width = canvas.width;
+	height = canvas.height;
+
+	drawCanvas();
+}
+
+window.addEventListener('resize', resizeCanvas);
+
+
+window.onload = function()
 {
 	canvas = document.getElementById("canvas");
 	if (!canvas)
@@ -261,25 +386,38 @@ export function makeGame()
 		return;
 	}
 
-	const pixelRatio = window.devicePixelRatio || 1;
-	canvas.width  = pixelRatio * canvas.clientWidth;
-	canvas.height = pixelRatio * canvas.clientHeight;
+	var tempWidth = window.screen.availWidth < window.innerWidth ? window.screen.availWidth : window.innerWidth;
+	var tempHeight = window.screen.availHeight < window.innerHeight ? window.screen.availHeight : window.innerHeight;
+
+	if (tempWidth < 1.5 * tempHeight)
+	{
+		canvas.width = tempWidth;
+		canvas.height = tempWidth / 1.5;
+	}
+	else
+	{
+		canvas.width = 1.5 * tempHeight;
+		canvas.height = tempHeight;
+	}
 
 	width = canvas.width;
 	height = canvas.height;
 
-	var offset = width / 30;
-	var halfHeight = height / 2 - height / 10;
-	p1 = new Player(offset, halfHeight, randomRGB(), height / 20, height / 5);
-	p2 = new Player(width - offset - (height / 20), halfHeight, randomRGB(), height / 20, height / 5);
-	ball = new Ball(
-		width / 2 + 5,
-		height / 2 + 5,
-		10,
-		10,
-		randomRGB(),
-		20
-	);
+	var playerWidth = height / 20;
+	var playerHeight = height / 5;
+	var startingP1X = width / 30;
+	var startingPY = height / 2 - playerHeight / 2;
+	var startingP2X = width - startingP1X - playerWidth;
+	var ballSize = height / 20;
+	var startingBallX = width / 2 - ballSize / 2;
+	var startingBallY = height / 2 - ballSize / 2;
+
+	p1 = new Player(startingP1X, startingPY, playerWidth, playerHeight);
+	p2 = new Player(startingP2X, startingPY, playerWidth, playerHeight);
+	ball = new Ball(startingBallX, startingBallY, 0, 0, "white", ballSize);
 	//document.addEventListener('keydown', moveP, false);
+ 	paused = true;
+	savedVel = 10;
+	savedAlpha = - (Math.PI / 4);
 	loop();
 }
